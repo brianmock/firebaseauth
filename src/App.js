25bcf98912144f25c3a0d2c485966b4816dd6acc
brianmock <briannealmock@gmail.com>
2016-11-28 17:15:34 -0600

    Complete login form with redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBadFq_AqIszJo9g82k-pYSn0X_5VFyqqo',
+      authDomain: 'manager-3a9e7.firebaseapp.com',
+      databaseURL: 'https://manager-3a9e7.firebaseio.com',
+      storageBucket: 'manager-3a9e7.appspot.com',
+      messagingSenderId: '881119198842'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
-    return(
-      <Provider store={createStore(reducers)}>
-        <View>
-          <Text>
-            Hello!
-          </Text>
-        </View>
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <LoginForm />
       </Provider>
     );
   }
